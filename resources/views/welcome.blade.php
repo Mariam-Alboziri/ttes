db97f6e145ad2fb75e3246f8a654b04cc18ec413
Mariam-Alboziri <102658576+Mariam-Alboziri@users.noreply.github.com>
2022-05-15 11:02:04 +0300

    layouts and create sections

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    @include('sections.container')
+
+    @include('sections.Products')
+
+    @include('sections.Sign')
+
+    @include('sections.why-us')
+
+ @endsection
